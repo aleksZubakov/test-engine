4180eb1384c5fd27c6ff3ab9dfe52230333f39b9
Aleksey Zubakov <aleks.zubakov@gmail.com>
2020-04-09 03:55:22 +0300

    Moved vector tests from main into vec2 mod

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,13 @@
+use std::fs::File;
+use std::io;
+
+use math::vec2::Vec2;
+
 mod primitives;
 mod intersect;
 mod math;
 
-use math::vec2::Vec2;
-use std::fs::File;
-use std::io;
-
 fn main() {
-    let t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Addition \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    println!("Test1 : {:#?}", &t1 + &t2);
-    println!("\n");
-
-    let t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Subtraction \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    println!("Test1 : {:#?}", &t1 - &t2);
-    println!("\n");
-
-    let t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Scalar product \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    println!("Test1 : {:#?}", &t1 ^ &t2);
-    println!("\n");
-
-    let t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Elementwise product \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    println!("Test1 : {:#?}", &t1 * &t2);
-    println!("\n");
-
-
-    let mut t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Addition with assignment \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    t1 += &t2;
-    println!("Test1 : {:#?}", t1);
-    println!("\n");
-
-//    t1 == t2;
-    let mut t1 = Vec2::create(1_f64, 2_f64);
-    let t2 = Vec2::create(1_f64, 2_f64);
-    println!("Subtraction with assignment \n t1: {:#?} \n t2: {:#?}", t1, t2);
-    t1 -= &t2;
-    println!("Test1 : {:#?}", t1);
-    println!("\n");
-
-    let t1 = Vec2::create(-1_f64, 2_f64);
-    println!("Magnitude  \n t1: {:#?}", t1);
-    println!("Test1 : {:#?}", t1.magnitude());
-    println!("\n");
-
-
+    println!("Hi there")
 }
 
--- src/math/vec2.rs
+++ src/math/vec2.rs
@@ -79,3 +79,89 @@ impl ops::Mul<&Vec2> for &Vec2 {
         self.el_product(other)
     }
 }
+
+impl PartialEq<Vec2> for Vec2 {
+    fn eq(&self, other: &Vec2) -> bool {
+        let Vec2(self_x, self_y) = self;
+        let Vec2(other_x, other_y) = self;
+
+
+        (self_x - other_x).abs() < std::f64::EPSILON
+            && (self_y - other_y).abs() < std::f64::EPSILON
+    }
+
+    fn ne(&self, other: &Vec2) -> bool {
+        unimplemented!()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::Vec2;
+
+    #[test]
+    pub fn sum() {
+        let t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Addition \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        println!("Test1 : {:#?}", &t1 + &t2);
+        assert_eq!(&t1 + &t2, Vec2(2_f64, 4_f64))
+    }
+
+    #[test]
+    pub fn subtraction() {
+        let t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Subtraction \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        println!("Test1 : {:#?}", &t1 - &t2);
+        assert_eq!(&t1 - &t2, Vec2(0_f64, 0_f64));
+    }
+
+    #[test]
+    pub fn scalar_product() {
+        let t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Scalar product \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        println!("Test1 : {:#?}", &t1 ^ &t2);
+        assert_eq!( &t1 ^ &t2, 5_f64);
+    }
+
+    #[test]
+    pub fn elementwise_product() {
+        let t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Elementwise product \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        println!("Test1 : {:#?}", &t1 * &t2);
+        assert_eq!(&t1 * &t2, Vec2::create(1_f64, 4_f64));
+    }
+
+
+    #[test]
+    pub fn assignment_with_addition() {
+        let mut t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Addition with assignment \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        t1 += &t2;
+        println!("Test1 : {:#?}", t1);
+        assert_eq!(t1, Vec2(2_f64, 4_f64));
+    }
+
+    #[test]
+    pub fn assignment_with_subtraction() {
+        let mut t1 = Vec2::create(1_f64, 2_f64);
+        let t2 = Vec2::create(1_f64, 2_f64);
+        println!("Subtraction with assignment \n t1: {:#?} \n t2: {:#?}", t1, t2);
+        t1 -= &t2;
+        println!("Test1 : {:#?}", t1);
+        assert_eq!(t1, Vec2::create(0_f64, 2_f64));
+    }
+
+    #[test]
+    pub fn magnituted() {
+        let t1 = Vec2::create(-1_f64, 2_f64);
+        println!("Magnitude  \n t1: {:#?}", t1);
+        println!("Test1 : {:#?}", t1.magnitude());
+        assert_eq!(t1.magnitude(), ((-1_f64).powi(2) + 2_f64.powi(2)).sqrt())
+    }
+}
